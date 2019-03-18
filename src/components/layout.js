1e1ca7a8af6e0990adde4074e89caccaa12daba1
Michał Worwąg <michalwo@gmail.com>
2019-03-18 19:40:35 +0100

    Add styled-components and react-icons, finish setup

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import { createGlobalStyle } from "styled-components";
 // import './bootstrap.min.css';
 // import "./layout.css";
-import "../sass/layout.scss";
+// import "../sass/layout.scss";
 
 const Layout = ({ children }) => {
-  return <React.Fragment>{children}</React.Fragment>;
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      {children}
+    </React.Fragment>
+  );
 };
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Open Sans', sans-serif;
+  color: #262626;
+  background: #ffffff;
+}
+`;
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaBeer } from "react-icons/fa";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
@@ -7,6 +8,10 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h3>Hello from Gatsby</h3>
+    <h3>
+      {" "}
+      Lets go for a <FaBeer />?{" "}
+    </h3>
   </Layout>
 );
 
