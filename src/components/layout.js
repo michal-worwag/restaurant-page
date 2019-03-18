50f4074e0f896410d3afe0a87330b1631116bfc1
Michał Worwąg <michalwo@gmail.com>
2019-03-18 19:03:01 +0100

    Add bootstrap

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,9 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from "react";
 import PropTypes from "prop-types";
 
-import "./layout.css";
+// import './bootstrap.min.css';
+// import "./layout.css";
+import "../sass/layout.scss";
 
 const Layout = ({ children }) => {
   return <React.Fragment>{children}</React.Fragment>;
