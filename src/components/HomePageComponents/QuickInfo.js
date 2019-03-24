bc67bb10fbf6df937150a428be041a9e8c669572
Michał Worwąg <michalwo@gmail.com>
2019-03-24 17:36:03 +0100

    Add SectionButton and finished QuickInfo component

--- src/components/HomePageComponents/QuickInfo.js
+++ src/components/HomePageComponents/QuickInfo.js
@@ -1,12 +1,43 @@
 import React, { Component } from "react";
-import { Section, Title } from "../../utils";
+import { Section, Title, SectionButton } from "../../utils";
+import styled from "styled-components";
+import { Link } from "gatsby";
+import { styles } from "../../utils";
 
 export default class QuickInfo extends Component {
   render() {
     return (
       <Section>
         <Title message="let us tell you" title="our mission" />
+        <QuickInfoWrapper>
+          <p className="tex">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+            tenetur sit facere inventore nam nesciunt adipisci est corrupti
+            officia, aliquam at a modi laudantium atque nihil reiciendis unde
+            architecto neque.
+          </p>
+          <Link to="/about/" style={{ textDecoration: "none" }}>
+            <SectionButton style={{ margin: "2rem auto" }}>About</SectionButton>
+          </Link>
+        </QuickInfoWrapper>
       </Section>
     );
   }
 }
+
+const QuickInfoWrapper = styled.div`
+  width: 90%;
+  margin: 2rem auto;
+  .text {
+    line-height: 2rem;
+    color: ${styles.colors.mainGrey};
+    word-spacing: 0.2rem;
+  }
+
+  @media (min-width: 768px) {
+    width: 70%;
+  }
+  @media (min-width: 992px) {
+    width: 60%;
+  }
+`;
